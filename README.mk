e8539987548fda7d2042ff7f7d51911e80ef860b
karljiang118440 <jiang_chaoqing@126.com>
2019-05-22 18:18:38 +0800

    build Tengine

---
+++ README.mk
@@ -0,0 +1,7 @@
+#一、环境搭建方法
+##1.1、
+ sudo apt install libprotobuf-dev protobuf-compiler libboost-all-dev libgoogle-glog-dev
+ sudo apt install libopencv-dev
+
+cd ~/tengine
+ cp makefile.config.example makefile.config
